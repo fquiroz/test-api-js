3cf127c1c79b6c1cf25df24e065718abac22c5b1
Fernando Quiroz <fquiroz@zafirus.tech>
2025-01-02 22:06:31 -0300

    new url

--- valen.js
+++ valen.js
@@ -2,12 +2,12 @@ const obtenerTareas = async () => {
     try {
         const opciones = {
             headers: {          
-                'Content-Type': 'application/json'
-            },
-            withCredentials: true
+                'Content-Type': 'application/json',
+              },
+            withCredentials: true,
         };
     
-        const respuesta = await axios.get('https://857e-38-52-83-229.ngrok-free.app/API/Tarea', opciones);      
+        const respuesta = await axios.get('https://fb92-38-52-83-229.ngrok-free.app/API/Tarea', opciones);      
         console.log('tareas de valen', respuesta.data);
         return respuesta.data;
     } catch (error) {
